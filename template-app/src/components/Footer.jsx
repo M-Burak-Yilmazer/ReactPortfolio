95a5ccc4da9d5104e62a10442ed0e2dad11ce783
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2023-12-01 13:23:07 +0300

    copy

--- template-app/src/components/Footer.jsx
+++ template-app/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import FooterStyle from "../scss/footer.module.scss";
 
+
 const Footer = () => {
   return (
     <footer className={FooterStyle["footer"]}>
-      <p>Copyright by Clarusway {new Date().getFullYear()}</p>
+      <p>
+        <span dangerouslySetInnerHTML={{ __html: "&copy;" }} /> Copyright by
+        Mathias {new Date().getFullYear()}
+      </p>
     </footer>
   );
 };
